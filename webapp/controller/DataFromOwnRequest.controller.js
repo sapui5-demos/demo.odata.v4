1120208a0355cbc1a752baf1d81e0eff96364289
D034877 <christoph.knecht@sap.com>
2018-09-14 21:24:00 +0000

    Initial commit

---
+++ webapp/controller/DataFromOwnRequest.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sap.training.demo.odata.v4.controller.DataFromOwnRequest", {
+
+		onPersonChange: function (oEvent) {
+			var oBindingContext = this.byId("peopleList").getSelectedItem().getBindingContext();
+			this.byId("tripList").setBindingContext(oBindingContext);
+		}
+
+	});
+
+});
