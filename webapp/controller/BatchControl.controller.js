5e9a858c9cc2e023b8d09d30315762755fc03794
D034877 <christoph.knecht@sap.com>
2018-11-16 13:25:56 +0000

    Batch Control

---
+++ webapp/controller/BatchControl.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("sap.training.demo.odata.v4.controller.BatchControl", {
+
+		onCancelPerson: function (oEvent) {
+			this.getView().getModel().resetChanges("PersonUpdateGroup");
+		},
+
+		onSavePerson: function (oEvent) {
+			this.getView().getModel().submitBatch("PersonUpdateGroup");
+		},
+
+		onShowPerson: function (oEvent) {
+			this.getView().getModel().submitBatch("PersonReadGroup");
+		},
+
+	});
+
+});
